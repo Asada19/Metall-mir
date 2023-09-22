d4b245d3a4c8be51975aa42785ff3fa31f6d2f5e
Asada19 <livencor@gmail.com>
2023-09-23 00:42:26 +0600

    some naming updates

--- shop/models.py
+++ shop/models.py
@@ -18,7 +18,7 @@ class Parameters(models.Model):
     catalog = models.OneToOneField('Catalog', on_delete=models.CASCADE, related_name='parameters')
 
     def __str__(self):
-        return f"{self.catalog.title}'s parameters"
+        return f"Параметры - {self.catalog.title}"
 
     class Meta:
         verbose_name = 'Парметр каталога товаров'
@@ -38,7 +38,7 @@ class Field(models.Model):
 
 
 class Item(models.Model):
-    title = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=100, blank=True, verbose_name='Название')
     catalog = models.ForeignKey('Catalog', on_delete=models.CASCADE, related_name='items', blank=True)
 
     def __str__(self):
@@ -52,7 +52,7 @@ class Item(models.Model):
 class ItemField(models.Model):
 
     item = models.ForeignKey('Item', on_delete=models.CASCADE, related_name='fields')
-    title = models.ForeignKey('Field', on_delete=models.CASCADE, max_length=100)
+    title = models.ForeignKey('Field', on_delete=models.CASCADE, max_length=100, verbose_name='Поле')
     value = models.CharField(max_length=100, blank=True, verbose_name='Значение')
 
     def __str__(self):
